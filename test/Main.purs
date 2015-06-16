69f8ba37ae17af922ddf49356bf227832e804b5f
U-CIQDEV\gbazerman <gbazerman@GBAZERMAN-T35.ciqdev.com>
2015-06-16 15:54:28 -0400

    add testing

---
+++ test/Main.purs
@@ -0,0 +1,43 @@
+module Test.Main where
+
+import Prelude
+import Data.Maybe
+import Data.Generics
+import Data.Array
+import Control.Monad.Eff.Console
+import Data.Either
+
+data Foo = Foo Number String | Bar Number | Quux (Array String) | Baz {a :: Maybe String, bq :: Number} String
+
+data IntList = IntList Number IntList | NilIntList
+
+instance genericFoo :: Generic Foo
+
+instance genericIntList :: Generic IntList
+
+instance showFoo :: Show Foo where
+    show = gShow
+
+instance eqFoo :: Eq Foo where
+    eq = gEq
+
+instance ordFoo :: Ord Foo where
+    compare = gCompare
+
+instance showIntList :: Show IntList where
+    show = gShow
+
+newtype MyNewString = MyNewString String
+
+instance genericNew :: Generic MyNewString
+
+instance showNewInt :: Show MyNewString where
+    show = gShow
+
+toFrom :: forall a. (Generic a) => a -> Maybe a
+toFrom x = fromSpine (toSpine x)
+
+main = do
+  print $ show $ toFrom [Foo 12.0 "Hello", Quux ["Hi","Dere"], Baz {a : Just "yo", bq : 22.0} "oy"]
+  print $ show $ gCompare (Bar 12.3) (Foo 34.1 "hello")
+  print $ show $ gEq [Foo 12.0 "Hello", Quux ["Hi","Dere"], Baz {a : Just "yo", bq : 22.0} "oy"] [Foo 12.0 "Hello", Quux ["Hi","Dere"], Baz {a : Just "yo", bq : 22.0} "yo"]
