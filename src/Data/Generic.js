98be844984cc9e5c702daed59f405f10f77e6487
Gary Burgess <gary.burgess@gmail.com>
2016-05-21 15:12:29 +0100

    Updates for PureScript 0.9

--- src/Data/Generic.js
+++ src/Data/Generic.js
@@ -1,47 +1,41 @@
-/* global exports */
 "use strict";
 
 // module Data.Generic
 
-
-//------------------------------------------------------------------------------
-// Zipping ---------------------------------------------------------------------
-//------------------------------------------------------------------------------
-
-exports.zipAll = function(f) {
-    return function(xs) {
-        return function(ys) {
-            var l = xs.length < ys.length ? xs.length : ys.length;
-            for (var i = 0; i < l; i++) {
-                if (!f(xs[i])(ys[i])) {
-                    return false;
-                }
-            }
-            return true;
-        };
+exports.zipAll = function (f) {
+  return function (xs) {
+    return function (ys) {
+      var l = xs.length < ys.length ? xs.length : ys.length;
+      for (var i = 0; i < l; i++) {
+        if (!f(xs[i])(ys[i])) {
+          return false;
+        }
+      }
+      return true;
     };
+  };
 };
 
-exports.zipCompare = function(f) {
-    return function(xs) {
-        return function(ys) {
-            var i = 0;
-            var xlen = xs.length;
-            var ylen = ys.length;
-            while (i < xlen && i < ylen) {
-                var o = f(xs[i])(ys[i]);
-                if (o !== 0) {
-                    return o;
-                }
-                i++;
-            }
-            if (xlen === ylen) {
-                return 0;
-            } else if (xlen > ylen) {
-                return -1;
-            } else {
-                return 1;
-            }
-        };
+exports.zipCompare = function (f) {
+  return function (xs) {
+    return function (ys) {
+      var i = 0;
+      var xlen = xs.length;
+      var ylen = ys.length;
+      while (i < xlen && i < ylen) {
+        var o = f(xs[i])(ys[i]);
+        if (o !== 0) {
+          return o;
+        }
+        i++;
+      }
+      if (xlen === ylen) {
+        return 0;
+      } else if (xlen > ylen) {
+        return -1;
+      } else {
+        return 1;
+      }
     };
+  };
 };
--- test/Main.purs
+++ test/Main.purs
@@ -1,42 +1,49 @@
 module Test.Main where
 
 import Prelude
-import Data.Maybe
-import Data.Generic
-import Data.Array
-import Control.Monad.Eff.Console
-import Data.Either
-import Test.Assert (assert', ASSERT)
-import Type.Proxy
 
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log, logShow)
 
-data Foo = Foo Number String | Bar Number | Quux (Array String) | Baz {a :: Maybe String, bq :: Number} String
-         | Corge (Array Char)
+import Data.Generic (class Generic, toSignature, gEq, gCompare, toSpine, fromSpine, gShow)
+import Data.Maybe (Maybe(..))
 
-data IntList = IntList Number IntList | NilIntList
+import Test.Assert (ASSERT, assert')
+import Type.Proxy (Proxy(..))
+
+data Foo
+  = Foo Number String
+  | Bar Number
+  | Quux (Array String)
+  | Baz {a :: Maybe String, bq :: Number} String
+  | Corge (Array Char)
+
+data IntList
+  = IntList Number IntList
+  | NilIntList
 
 derive instance genericFoo :: Generic Foo
 
 derive instance genericIntList :: Generic IntList
 
 instance showFoo :: Show Foo where
-    show = gShow
+  show = gShow
 
 instance eqFoo :: Eq Foo where
-    eq = gEq
+  eq = gEq
 
 instance ordFoo :: Ord Foo where
-    compare = gCompare
+  compare = gCompare
 
 instance showIntList :: Show IntList where
-    show = gShow
+  show = gShow
 
 newtype MyNewString = MyNewString String
 
 derive instance genericNew :: Generic MyNewString
 
 instance showNewInt :: Show MyNewString where
-    show = gShow
+  show = gShow
 
 toFrom :: forall a. (Generic a) => a -> Maybe a
 toFrom x = fromSpine (toSpine x)
@@ -44,14 +51,15 @@ toFrom x = fromSpine (toSpine x)
 data Bar = Bar1 Int
 derive instance genericBar :: Generic Bar
 
+main :: Eff (console :: CONSOLE, assert :: ASSERT) Unit
 main = do
-  print $ show $ toFrom [
+  logShow $ toFrom [
     Foo 12.0 "Hello"
   , Quux ["Hi","Dere"]
   , Baz {a : Just "yo", bq : 22.0} "oy"
   , Corge ['H', 'i', ' ', 'D', 'e', 'r', 'e'] ]
-  print $ show $ gCompare (Bar 12.3) (Foo 34.1 "hello")
-  print $ show $ gEq
+  logShow $ gCompare (Bar 12.3) (Foo 34.1 "hello")
+  logShow $ gEq
     [ Foo 12.0 "Hello"
     , Quux ["Hi","Dere"]
     , Baz {a : Just "yo", bq : 22.0} "oy"
@@ -62,14 +70,16 @@ main = do
     , Corge ['H', 'i', ' ', 'D', 'e', 'r', 'e'] ]
 
   log "Testing Show GenericSignature instance:"
-  print (toSignature (Proxy :: Proxy Number))
-  print (toSignature (Proxy :: Proxy Char))
-  print (toSignature (Proxy :: Proxy (Array (Array Number))))
-  print (toSignature (Proxy :: Proxy Foo))
-  print (toSignature (Proxy :: Proxy (Array Foo)))
-  print (toSignature (Proxy :: Proxy MyNewString))
+  logShow (toSignature (Proxy :: Proxy Number))
+  logShow (toSignature (Proxy :: Proxy Char))
+  logShow (toSignature (Proxy :: Proxy (Array (Array Number))))
+  logShow (toSignature (Proxy :: Proxy Foo))
+  logShow (toSignature (Proxy :: Proxy (Array Foo)))
+  logShow (toSignature (Proxy :: Proxy MyNewString))
+  logShow (toSignature (Proxy :: Proxy Ordering))
   log "Basic test of Eq GenericSignature instance .."
   assert' "Foo == Foo:" $ toSignature (Proxy :: Proxy Foo) == toSignature (Proxy :: Proxy Foo)
-  assert' "Foo == Bar:" $ toSignature (Proxy :: Proxy Foo) /= toSignature (Proxy :: Proxy Bar)
-  assert' "Foo == Int:" $ toSignature (Proxy :: Proxy Foo) /= toSignature (Proxy :: Proxy Int)
-  assert' "Foo == Array Int:" $ toSignature (Proxy :: Proxy Foo) /= toSignature (Proxy :: Proxy (Array Int))
+  assert' "Foo /= Bar:" $ toSignature (Proxy :: Proxy Foo) /= toSignature (Proxy :: Proxy Bar)
+  assert' "Foo /= Int:" $ toSignature (Proxy :: Proxy Foo) /= toSignature (Proxy :: Proxy Int)
+  assert' "Foo /= Array Int:" $ toSignature (Proxy :: Proxy Foo) /= toSignature (Proxy :: Proxy (Array Int))
+  assert' "Foo /= Ordering:" $ toSignature (Proxy :: Proxy Foo) /= toSignature (Proxy :: Proxy Ordering)
